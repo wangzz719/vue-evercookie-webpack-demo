67234e0be9b11904402db4da3c4fbef3e1072fba
wangzhizhao <wangzhizhao@zhihu.com>
2018-02-12 11:03:55 +0800

    init

---
+++ webapp/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{message}}</div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {message: 'This is an vue webpack demo'}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
